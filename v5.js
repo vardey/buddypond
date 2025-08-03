5e99194d4d0493027be9c172c09915e854dbb9fd
Marak <marak.squires@gmail.com>
2025-08-02 22:37:45 -1000

    Switch to production mode

--- v5.js
+++ v5.js
@@ -51,7 +51,6 @@ const DEV_ENDPOINTS = {
 // Initialize application configuration based on environment
 function configureEnvironment() {
   devmode = window.location.hostname !== 'buddypond.com';
-  devmode = true;
   if (devmode) {
     return DEV_ENDPOINTS
   }
