764aee07b36cdb3912e6d407e319673448b88dc3
Marak <marak.squires@gmail.com>
2025-09-07 19:56:07 -1000

    `markdown-card` should scrollToBottom on load Updates `markdown-card` to use new rainbow CSS class

--- v5/apps/based/card/cards/markdown/markdown-card.js
+++ v5/apps/based/card/cards/markdown/markdown-card.js
@@ -1,7 +1,10 @@
-export default function applyData(el, data) {
+export default function applyData(el, data, cardClass, parent) {
     let $el = $(el);
     $el.find('.card-md-close-btn').on('click', () => {
         $el.hide(); // Hide the help card (replace with your preferred close logic)
         // set local storage 'viewed-help-card' to true
     });
+    if (cardClass.bp && cardClass.bp.apps && cardClass.bp.apps.buddylist) {
+      cardClass.bp.apps.buddylist.scrollToBottom(parent.content);
+    }
 }
